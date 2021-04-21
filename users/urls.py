bc8ebd86f323a5806ace07de85a23c41be3bd9c7
Carlos <chjure@gmail.com>
2021-04-21 20:44:33 -0300

    Redirecciona

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 from . import views
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/dashboard/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
